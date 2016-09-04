bc44901841f14ee7a341432eb70ed0580afbc346
lincenying <lincenying@qq.com>
2016-09-04 14:38:07 +0800

    editor lib url

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -47,7 +47,7 @@ export default {
             height: 500,
             markdown: "",
             placeholder: '请输入内容...',
-            path: './static/editor.md/lib/',
+            path: '/static/editor.md/lib/',
             toolbarIcons() {
                 return [
                     "bold", "italic", "quote", "|",
